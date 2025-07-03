3262cb43299204b176ce261318daa19215913163
Manu Alpuente <manualpuente@gmail.com>
2025-07-03 15:37:03 +0200

    Company financials

---
+++ src/modules/CompanyFinancials/index.js
@@ -0,0 +1,50 @@
+import "./CompanyFinancials.scss";
+
+const CompanyFinancials = ({ data }) => {
+  console.log("Company-Financials", { data });
+  return (
+    <div className="company-financials">
+      <h2 className="page-section-title">Company financials</h2>
+      <span className="company-financials__score">{data.score}</span>
+      <p className="company-financials__label">Score</p>
+      <div className="company-financials__data-items">
+        <div className="company-financials__data-item">
+          <span className="company-financials__data-value">
+            {data.turnover}
+          </span>
+          <p className="company-financials__data-key">Turnover</p>
+        </div>
+        <div className="company-financials__data-item">
+          <span className="company-financials__data-value">{data.ebitda}</span>
+          <p className="company-financials__data-key">EBITDA</p>
+        </div>
+        <div className="company-financials__data-item">
+          <span className="company-financials__data-value">
+            {data.cagr_turnover_5y}
+          </span>
+          <p className="company-financials__data-key">5Y T/O CAGR</p>
+        </div>
+        <div className="company-financials__data-item">
+          <span className="company-financials__data-value">
+            {data.cagr_ebitda_5y}
+          </span>
+          <p className="company-financials__data-key">5Y EBITDA CAGR</p>
+        </div>
+        <div className="company-financials__data-item">
+          <span className="company-financials__data-value">
+            {data.ebitda_margin}
+          </span>
+          <p className="company-financials__data-key">EBITDA Margin</p>
+        </div>
+        <div className="company-financials__data-item">
+          <span className="company-financials__data-value">
+            {data.nfd_ebitda}
+          </span>
+          <p className="company-financials__data-key">NFD/EBITDA</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyFinancials;
--- src/modules/PageCompany/index.js
+++ src/modules/PageCompany/index.js
@@ -1,11 +1,17 @@
+import CompanyFinancials from "../CompanyFinancials";
 import CompanyInfo from "../CompanyInfo";
+import "./PageCompany.scss";
 
 const PageCompany = ({ data }) => {
+  console.log("PageCompany", { data });
   return (
     <div className="container">
       <div className="row row_company-page">
         <div className="col">
           <CompanyInfo data={data.company} />
+        </div>
+        <div className="col">
+          <CompanyFinancials data={data.financials} />
         </div>
       </div>
     </div>
